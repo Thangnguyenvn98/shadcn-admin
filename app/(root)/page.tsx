971a83041f9030497eb70e8e5bfc35cb7b3a65d5
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-07-04 19:35:30 -0400

    Adding state management zustand for modal and made it not possible to not create a store if one doesn't exist

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,24 @@
 "use client"
 
 import { Modal } from "@/components/ui/modal"
+import { useStoreModal } from "@/hooks/use-store-modal"
+import { useEffect } from "react";
 
 export default function SetupPage() {
+    //Seperate it out so it works better in the useEffect as the original one will not be working nicely
+    const onOpen = useStoreModal((state)=> state.onOpen);
+    const isOpen = useStoreModal((state)=> state.isOpen);
+
+    useEffect(()=>{
+      if(!isOpen) {
+        onOpen();
+      }
+    }
+    ,[isOpen, onOpen])
+
     return (
       <div className="p-4">
-        <Modal title="Test" description="Test Desc" isOpen onClose={()=>{}}>
-          Children
-        </Modal>
+        Root Page
       </div>
     )
   }
