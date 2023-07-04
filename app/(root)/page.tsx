1fa563b520ca2113cfe089d24e49edef7dceee2c
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-07-04 19:14:38 -0400

    Importing Dialog from shadcn and installing zustand npm

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client"
+
+import { Modal } from "@/components/ui/modal"
 
 export default function SetupPage() {
     return (
       <div className="p-4">
-        <UserButton afterSignOutUrl="/"/>
+        <Modal title="Test" description="Test Desc" isOpen onClose={()=>{}}>
+          Children
+        </Modal>
       </div>
     )
   }
